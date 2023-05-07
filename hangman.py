75bfcf86bce865ce5830f2d8db19d19e30677efd
Patrick-Apilukpuvadol <chimei983@hotmail.com>
2023-05-07 19:49:07 +1000

    removed some duplicate code

--- hangman.py
+++ hangman.py
@@ -8,19 +8,6 @@ from colored import fg, bg, attr
 import string
 from functions import win_game, lose_game, user_input, input_incorrect, guess_wrong
 
-def win_game(mystery_word, partial_answer):
-    print(f"{fg(10)}You have won!!! Congratulations!{attr(0)} The Mystery word was: {fg(11)} {mystery_word}{attr(0)}")
-    save_name = input('Enter your name. ').title().upper()
-    save_score = input('Enter the attempts you have left. ')
-
-    text_file = open("highscores.txt", "a")
-    text_file.write("\n" + save_name + ' has won 1 round with ' + save_score + ' attempts remaining' + "\n")
-    text_file.close()
-    
-def lose_game(mystery_word, partial_answer):
-    print(f"{fg(1)}You have lost...The Mystery word was: {attr(0)} {fg(11)}{mystery_word}{attr(0)}")
-
-
 replay = True
 while replay:
 #Using Nested loop to facilitate replayability in the game
